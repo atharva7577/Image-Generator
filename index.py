c7d93f5bad93b4c8b0d59af9cb353e261c3fc3b8
atharva7577 <63158390+atharva7577@users.noreply.github.com>
2024-02-21 22:31:49 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,23 +1,23 @@
 from flask import Flask, jsonify
-from config import key
-import openai
+#from config import key
+#import openai
 from flask import render_template
 
-openai.api_key = key
+#openai.api_key = key
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-  return render_template('/Dalle-Image Generator/templates/index.html', )
+  return "Hello World!"
   
-@app.route('/generateimages/<prompt>')
+'''@app.route('/generateimages/<prompt>')
 def generate(prompt):
   print("prompt:", prompt)
   response = openai.Image.create(prompt=prompt, n=5, size="256x256") 
   print(response)
-  return jsonify(response)
+  return jsonify(response)'''
 
 
 app.run(host='0.0.0.0', port=81)
